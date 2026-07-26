c8cd417e526a9dc24bac229499f3a74cc9a9717b
Roshan <pahariroshan55@gmail.com>
2026-07-26 10:01:00 +0545

    Add motion library and update App component for animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
- 
+import { motion } from "motion/react";
 const App = () => {
   return (
-    <>
-      <div className="bg-black w-full">
+    <div className="bg-black w-full h-screen">
+      <motion.div
+      className="h-100 w-100 bg-amber-400 border-2 border-amber-100"
+      animate={{ 
+        rotate: 360,
+        x:800,
+       
+      }}
+      transition={{
+        duration:3,
+        delay:1
+      }}
+      >
 
-      </div>
-    </>
-  )
-}
+      </motion.div>
+    </div>
+  );
+};
 
-export default App
+export default App;
