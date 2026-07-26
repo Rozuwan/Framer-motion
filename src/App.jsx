d649e742786aa75e37bf40159473f38dcdc1d8f6
Roshan <pahariroshan55@gmail.com>
2026-07-26 10:39:15 +0545

    Update motion properties for enhanced animation effects in App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,22 @@ const App = () => {
     
       <div className="bg-black h-screen p-3">
           <motion.div
+          initial={{
+            x:0,
+            y:0
+          }}
           animate={{
-            rotate:360,
-            x:300
+            x:[0,800,800,0,0],
+            y:[0,0,400,400,0],
+            rotate:500
           }}
           transition={{
-            delay:3,
-            duration:3
+            delay:1,
+            duration:3,
+            repeat:Infinity
+            
           }}
-           className="h-80 w-80 bg-amber-300"
+           className="h-80 w-80 bg-amber-300 border-2 border-amber-50 "
            >
           </motion.div>
       </div>
