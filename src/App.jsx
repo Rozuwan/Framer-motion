d4d038922962e871b4c068bdf53206bf146d9074
Roshan <pahariroshan55@gmail.com>
2026-07-29 04:37:07 +0545

    Refactor boxVariants in App component and remove unused hover and tap animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,32 @@
 import { motion } from "motion/react";
 const App = () => {
-  const boxVariants = {
-   initial:{
-    scale:0,
-    skew:0,
-   },
-   hover:{
-    scale:0.5,
-    rotate:6,
-    skew:'10deg',
-    transition:{duration:0.3}
-   },
-   click:{
-    scale:0.9,
-    rotate:15
-   }
-  };
+ 
   return (
     <div className="bg-black h-screen p-3">
       <motion.div
         className="h-80 w-80 bg-amber-300 border-2 rounded-lg"
-       variants={boxVariants}
-       whileTap='click'
-       whileHover='hover'
+        // whileHover={{
+        //   backgroundColor:'red',
+        //   scale:0.5
+        // }}
+
+        // whileTap={{
+        //   backgroundColor:'pink',
+        //   rotate:50
+        // }}
+        drag
+        whileDrag={{
+          scale:1.2,
+          backgroundColor:'red',
+        }}
+        transition={{
+          type:'spring',
+          stiffness:1000
+        }}
       ></motion.div>
     </div>
   );
 };
 
 export default App;
+``
