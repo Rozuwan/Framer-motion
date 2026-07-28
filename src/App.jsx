4a298000edb853a6d4d6ac426dde7c917fa4ffeb
Roshan <pahariroshan55@gmail.com>
2026-07-28 20:58:00 +0545

    Refactor boxVariants in App component for improved animation effects

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,28 @@
 import { motion } from "motion/react";
 const App = () => {
   const boxVariants = {
-    initial: {
-      scale: 1,
-      rotate: 0,
-      skew: 0,
-    },
-    hover: {
-      scale: 0.5,
-      rotate: 15,
-      skew: "10deg",
-      transition: { duration: 0.3 },
-    },
-    click: { scale: 0.9, rotate: -15, transition: { duration: 0.3 } },
+   initial:{
+    scale:0,
+    skew:0,
+   },
+   hover:{
+    scale:0.5,
+    rotate:6,
+    skew:'10deg',
+    transition:{duration:0.3}
+   },
+   click:{
+    scale:0.9,
+    rotate:15
+   }
   };
   return (
     <div className="bg-black h-screen p-3">
       <motion.div
         className="h-80 w-80 bg-amber-300 border-2 rounded-lg"
-        variants={boxVariants}
-        initial="initial"
-        whileHover="hover"
-        whileTap="click"
+       variants={boxVariants}
+       whileTap='click'
+       whileHover='hover'
       ></motion.div>
     </div>
   );
